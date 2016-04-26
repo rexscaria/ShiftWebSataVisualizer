d5d016e86d63a5fde818eb556a48341a90a453c0
Scaria <arunscaria91@gmail.com>
2016-04-26 00:17:06 -0700

    Heroku

--- web.js
+++ web.js
@@ -11,8 +11,7 @@ var app            = express();
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser()); // pull information from html in POST
 app.use(methodOverride()); 
-app.use(express.static(__dirname + '/app'));
 //add this so the browser can GET the bower files
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+//app.use('/bower_components', express.static(__dirname + '/bower_components'));
+//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
