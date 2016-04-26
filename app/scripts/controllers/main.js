0ef128f9129e43c5c74eb02d2652e57a1f21bf71
Scaria <arunscaria91@gmail.com>
2016-04-25 23:24:59 -0700

    Heroku

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,32 +10,32 @@
 angular.module('atApp')
   .controller('MainCtrl', function ($scope, $firebaseArray) {
 
-  	$scope.usersref = new Firebase("https://swiftbalanceapp.firebaseio.com/users");
+  	$scope.usersref = new Firebase('https://swiftbalanceapp.firebaseio.com/users');
   	$scope.users = $firebaseArray($scope.usersref);
 
   	$scope.setUser = function(user){
-  		$scope.user = user
-  	}
+  		$scope.user = user;
+  	};
 
-  	$scope.exerciseref = new Firebase("https://swiftbalanceapp.firebaseio.com/exercises");
+  	$scope.exerciseref = new Firebase('https://swiftbalanceapp.firebaseio.com/exercises');
   	$scope.exercises = $firebaseArray($scope.exerciseref);
 
   	$scope.setExercise = function(exercise){
-  		$scope.exercise = exercise
-  	}
+  		$scope.exercise = exercise;
+  	};
 
   	$scope.draw = function(){
-  		var di = $scope.exercise[$scope.user.$id]
+  		var di = $scope.exercise[$scope.user.$id];
 
-  		var xVals = Object.keys(di)
-  		var yVals = Object.keys(di).map(function(key){return [new Date(key*1000), String(di[key])]})
+  		var xVals = Object.keys(di);
+  		var yVals = Object.keys(di).map(function(key){return [new Date(key*1000), String(di[key])]});
   		//console.log(yVals)
 
   		new Dygraph(
-          document.getElementById("graph"),
+          document.getElementById('graph'),
           yVals,
           {
-            title: $scope.exercise.$id +" - "+$scope.user.$id,
+            title: $scope.exercise.$id +' - '+$scope.user.$id,
             labels:['Time', 'Score'],
             labelsDivStyles: { 'textAlign': 'right' },
             showRangeSelector: true,
@@ -44,6 +44,6 @@ angular.module('atApp')
             valueRange: [0,100]
           }
       );
-  	}
+  	};
   	//console.log($scope.exercises)
   });
